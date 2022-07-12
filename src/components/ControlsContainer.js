d2bb2a72a4bd1257638a416f1dec0858f6796c86
kolawole olaniyi <kolaniyi3@gmail.com>
2022-07-12 17:42:13 +0100

    refactor app so filter and expense prop can be manage by the same component

--- src/components/ControlsContainer.js
+++ src/components/ControlsContainer.js
@@ -1,6 +1,20 @@
 import React, { useState, useEffect } from "react";
+import { useQuery, gql } from "@apollo/client";
 import FiltersComponent from "./FiltersComponent";
 import ExpensesContainer from "./expenses/ExpensesContainer";
+
+const GET_EXPENSES = gql`
+  query {
+    allExpenses {
+      id
+      createdAt
+      name
+      description
+      cost
+      status
+    }
+  }
+`;
 
 const ControlsContainer = () => {
   const [listFilters, setListFilters] = useState({
@@ -10,9 +24,29 @@ const ControlsContainer = () => {
     endcost: "",
   });
 
+  const { loading, error, data } = useQuery(GET_EXPENSES);
+  const [groupedExpense, setGroupedExpense] = useState();
+
+  useEffect(() => {
+    if (data) {
+      const objMap = {};
+      data.allExpenses.forEach((expense) => {
+        let key = expense.createdAt;
+        if (!objMap[key]) {
+          objMap[key] = [];
+        }
+        objMap[key].push(expense);
+      });
+      setGroupedExpense(Object.entries(objMap));
+    }
+  }, [data]);
+
   useEffect(() => {
     console.log(listFilters);
   }, [listFilters]);
+
+  if (error) return error;
+  if (loading) return loading;
 
   return (
     <>
@@ -21,7 +55,7 @@ const ControlsContainer = () => {
           listFilters={listFilters}
           setListFilters={setListFilters}
         />
-        <ExpensesContainer />
+        <ExpensesContainer groupedExpense={groupedExpense} />
       </div>
     </>
   );
--- src/components/expenses/ExpensesContainer.js
+++ src/components/expenses/ExpensesContainer.js
@@ -1,47 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container } from "reactstrap";
-import { useQuery, gql } from "@apollo/client";
 
 import ExpenseListItem from "./ExpenseListItem";
-
-const GET_EXPENSES = gql`
-  query {
-    allExpenses {
-      id
-      createdAt
-      name
-      description
-      cost
-      status
-    }
-  }
-`;
 
 const parseDateAsStandardDate = (randomTime) => {
   return randomTime.split("-").reverse().join("-");
 };
 
-const ExpensesContainer = () => {
-  const { loading, error, data } = useQuery(GET_EXPENSES);
-  const [groupedExpense, setGroupedExpense] = useState();
-
-  useEffect(() => {
-    if (data) {
-      const objMap = {};
-      data.allExpenses.forEach((expense) => {
-        let key = expense.createdAt;
-        if (!objMap[key]) {
-          objMap[key] = [];
-        }
-        objMap[key].push(expense);
-      });
-      setGroupedExpense(Object.entries(objMap));
-    }
-  }, [data]);
-
-  if (error) return error;
-  if (loading) return loading;
-
+const ExpensesContainer = ({ groupedExpense }) => {
   return (
     <Container>
       <div className="background-container pt-4 pb-4  mb-2">
